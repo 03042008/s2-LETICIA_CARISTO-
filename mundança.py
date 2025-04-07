7288ef391a7a9f4da155adf38d5bae4331d93327
03042008 <leticia.c.santos27@aluno.senai.br>
2025-04-07 14:15:37 -0300

    mudança
    houve uma mudança nesse programa

--- mundança.py
+++ mundança.py
@@ -2,15 +2,13 @@ PRINT("Solicitar a idade de várias pessoas e imprimir: Total de pessoas com men
 21 anos. Total de pessoas com mais de 50 anos. O programa termina quando idade")
 for = -99.
 print ("me diga algumas idades e direi quantas pessoas tem menos de 21 e mais de 50 amos")
-menor_21= 0
 maior_50=0
 while True:
     idade=int(("me diga sua idade"))
     if idade == -99:
         print("voce escolheu sair")
         break
-    elif idade< 21:
         menor21= menor_21+1
     if >50:
-        maior_50=maior_50+1
-print(f" o total de pessoas com idade menor que 21 é {menor_21} e pessoas com mais de 50 é de {maior_50}")
+        maior_50=_50+1
+print(f" o total de pessoas com idade menor que 21 e pessoas com mais de 50 é de {maior_50}")
